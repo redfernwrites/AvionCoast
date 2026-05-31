5523d05585c787197677d929ed716a057a0aa397
Kate <200~288068484+redfernwrites@users.noreply.github.com>
2026-05-31 14:59:59 -0300

    CI(site): update the site properties
    Updated the copyright statement and changed the html theme to sphinx_rtd_theme.

--- site/conf.py
+++ site/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Avion Coast Documentation'
-copyright = '2026, Kate Redfern'
+copyright = '2025 - 2026, Kate Redfern'
 author = 'Kate Redfern'
 
 # -- General configuration ---------------------------------------------------
@@ -23,5 +23,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
